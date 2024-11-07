394420a0e383f8336976dca83fb84d1de62ff874
imogenemslt <144333984+imogenemslt@users.noreply.github.com>
2024-11-07 11:02:22 +0000

    Update task1_pg_250.py

--- task1_pg_250.py
+++ task1_pg_250.py
@@ -13,4 +13,30 @@ db.writerow(data3)
 
 
 
+import csv
+# heading = ["make","model","reg"]
+# file = open("mygarage.csv","a",newline = "")
+# db = csv.writer(file)
+# db.writerow(heading)
+# file.close()
+# data = []
+# data1 = input("what make is the vehicle: ")
+# data.append(data1)
+# data2 = input("what model is the vehicle: ")
+# data.append(data2)
+# data3 = input("what reg is the vehicle: ")
+# data.append(data3)
+# file = open("mygarage.csv","a",newline = "")
+# db = csv.writer(file)
+# db.writerow(data)
+# file.close()
+# 
+#
 
+file = open("mygarage.csv","r")
+data = list(csv.reader(file))
+file.close()
+print("make\t model\t\t reg\t")
+print("-----------------------------------------")
+for i in data[1:]:
+    print(data[1],data[2],data[3])
