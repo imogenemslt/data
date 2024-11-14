4e248c8275086b7d8c64370b8dcc4b37a5a5a48d
imogenemslt <144333984+imogenemslt@users.noreply.github.com>
2024-11-14 11:00:28 +0000

    Update database_test.py

--- database_test.py
+++ database_test.py
@@ -25,18 +25,24 @@ mycursor = mydb.cursor()
 #mycursor.execute("CREATE TABLE patient (id INT AUTO_INCREMENT PRIMARY KEY, fname VARCHAR(100), lname VARCHAR(100), dob date, age int(11),phone VARCHAR(55), doctor_id int(11))")
 #mycursor.execute("CREATE TABLE doctor (id INT AUTO_INCREMENT PRIMARY KEY, fname VARCHAR(100), lname VARCHAR(100))")
 
-choice = int(input("do you want to enter doc(1) or patient(2)?": ))
+choice = int(input("do you want to enter doc(1) or patient(2)?"))
 if choice == 1:
     DFname = input("enter doctors first name: ")
     DLname = input("enter last name: ")
+    docid =""
+    insertDoc = "INSERT INTO doctor (fname, lname) VALUES (%s, %s)"
+    docval = (DFname, DLname)
+    mycursor.execute(insertDoc, docval)
 else:
     PFname = input("enter patient first name: ")
     Plname = input("enter patient last name: ")
-    dob = input("enter patients date of birth: ")
+    dob = input("enter patients date of birth(yyyy-mm-dd): ")
     age = input("enter patients age: ")
     phone = input("enter patients phone number: ")
     docId = input("enter your doctors id")
-  sql = "INSERT INTO patient (PFname, Plname,dob,age,phone,docId) VALUES (%s, %s)"
-  sql = "INSERT INTO patient (DFname, Dlname) VALUES (%s, %s)"
-   mycursor.execute(sql)
-  mydb.commit()
+    insertPat = "INSERT INTO patient (fname, lname, dob, age, phone, doctor_id) VALUES (%s, %s, %s, %s, %s, %s)"
+    valpat = (PFname, Plname,dob,age,phone,docId)
+    mycursor.execute(insertPat,valpat)
+
+mydb.commit()
+#INSERT INTO `patient` (`id`, `fname`, `lname`, `dob`, `age`, `phone`, `doctor_id`) VALUES (NULL, 'imogne', 'murphy', '2024-11-13', '14', '4738
